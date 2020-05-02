ca8e18562a15439e99ad8224aa254a21755536a1
Aaron Pradhan <apradhan@tripadvisor.com>
2020-05-02 18:07:54 -0400

    Finish implementing runGame

--- src/BackendNET.java
+++ src/BackendNET.java
@@ -62,11 +62,11 @@ public class BackendNET {
    * so the method is the same for converting from client to server row numbers as server to
    * client row numbers.
    */
-  private int convertRowNum(int clientRowNum) {
+  private static int convertRowNum(int clientRowNum) {
     return (NUM_ROWS - 1) - clientRowNum;
   }
 
-  private char[][] getInitialBoard() {
+  private static char[][] getInitialBoard() {
     char[][] board = new char[NUM_COLUMNS][NUM_ROWS];
     for (int col = 0; col < NUM_COLUMNS; col++) {
       for (int row = 0; row < NUM_ROWS; row++) {
@@ -76,7 +76,7 @@ public class BackendNET {
     return board;
   }
 
-  private boolean isBoardFull(char[][] board) {
+  private static boolean isBoardFull(char[][] board) {
     for (int col = 0; col < NUM_COLUMNS; col++) {
       for (int row = 0; row < NUM_ROWS; row++) {
         if (board[col][row] != 'N') {
@@ -87,12 +87,74 @@ public class BackendNET {
     return true;
   }
 
+  private static boolean isColumnFull(char[][] board, int column) {
+    for (char cell : board[column]) {
+      if (cell == 'N') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean isLegalMove(char[][] board, int column) {
+    if (column < 0 || column > NUM_COLUMNS) {
+      return false;
+    }
+    return !isColumnFull(board, column);
+  }
+
+  /**
+   * Returns the row in which the token was placed.
+   */
+  private static int placePlayerToken(char[][] board, char player, int column) {
+    // getRowNum returns the highest row with a token in it, so must add 1 to get blank spot
+    int row = Connect4WinChecker.getRowNum(board, column) + 1;
+    board[column][row] = player;
+    return row;
+  }
+
+  /**
+   * Returns true if the player won, false otherwise.
+   */
+  private static boolean playTurn(char[][] board, IClient activePlayer, IClient opponent,
+      char activePlayerColor) {
+    activePlayer.setPlayerTurn(activePlayerColor);
+    opponent.setPlayerTurn(activePlayerColor);
+    int column = activePlayer.getDropColumn();
+    while (!isLegalMove(board, column)) {
+      column = activePlayer.getDropColumn();
+    }
+    int row = placePlayerToken(board, activePlayerColor, column);
+    activePlayer.registerPlayerDrop(activePlayerColor, column, convertRowNum(row));
+    opponent.registerPlayerDrop(activePlayerColor, column, convertRowNum(row));
+    if (Connect4WinChecker.checkWin(board, column)) {
+      activePlayer.gameOver(activePlayerColor);
+      opponent.gameOver(activePlayerColor);
+      return true;
+    }
+    return false;
+  }
+
   public void runGame(IClient client1, IClient client2) {
     char[][] board = getInitialBoard();
     client1.setPlayerColor(CLIENT1_COLOR);
     client2.setPlayerColor(CLIENT2_COLOR);
-    while (!isBoardFull(board) && )
-
+    boolean isWin = false;
+    while (!isBoardFull(board)) {
+     isWin = playTurn(board, client1, client2, CLIENT1_COLOR);
+      if (isWin) {
+        break;
+      }
+      isWin = playTurn(board, client2, client1, CLIENT2_COLOR);
+      if (isWin) {
+        break;
+      }
+    }
+    // can have board full and win at the same time, so need to store isWin boolean
+    if (!isWin) {
+      client1.gameOver('N');
+      client2.gameOver('N');
+    }
   }
 
   /**
