6e822c9a924fbe50c4400b7622531f2ad1d8f16a
Aaron Pradhan <apradhan@tripadvisor.com>
2020-05-02 18:55:04 -0400

    Switch to try resource statement

--- src/sockets/Server.java
+++ src/sockets/Server.java
@@ -6,57 +6,40 @@ import java.net.*;
 
 public class Server {
 
+    private static final int PORT_NUM = 11113;
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        // Port number to bind server to.
-        int portNum = 11113;
-        
-        // Socket for server to listen at.
-        ServerSocket listener = new ServerSocket(portNum);
+        try (ServerSocket listener = new ServerSocket(PORT_NUM)) {
+            System.out.println("Server is now running at port: " + PORT_NUM);
+            // Accept a client connection once Server recieves one.
+            Socket clientSocket = listener.accept();
+            Socket clientSocket2 = listener.accept();
 
-        System.out.println("Server is now running at port: " + portNum);
+            // Creating inout and output streams. Must creat out put stream first.
+            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+            ObjectOutputStream out2 = new ObjectOutputStream(clientSocket2.getOutputStream());
+            ObjectInputStream in2 = new ObjectInputStream(clientSocket2.getInputStream());
 
-        // Simply making Server run continously.
-        while (true) {
-            try {
-                // Accept a client connection once Server recieves one.
-                Socket clientSocket = listener.accept();
-                Socket clientSocket2 = listener.accept();
+            // Reading in Integer Object from input stream.
+            int i = (Integer) in.readObject();
+            int i2 = (Integer) in2.readObject();
 
+            //Sending response back to client
+            String response = "Integer Object Received.";
+            out.writeObject(response);
+            out2.writeObject(response);
 
-                // Creating inout and output streams. Must creat out put stream first.
-                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-                ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-                ObjectOutputStream out2 = new ObjectOutputStream(clientSocket2.getOutputStream());
-                ObjectInputStream in2 = new ObjectInputStream(clientSocket2.getInputStream());
-
-                // Reading in Integer Object from input stream.
-                int i = (Integer) in.readObject();
-                int i2 = (Integer) in2.readObject();
-
-                //Sending response back to client
-                String response = "Integer Object Received.";
-                out.writeObject(response);
-                out2.writeObject(response);
-
-                // Outputting recieved Integer Object.
-                System.out.println("Received integer: " + i);
-                System.out.println("Received integer: " + i2);
-                out.close();
-                in.close();
-                out2.close();
-                in2.close();
-                clientSocket.close();
-                clientSocket2.close();
-                break;
-            } finally {
-                      // Closing Server Socket now.
-                      listener.close();
-               
-            }
-            
-             
-                
+            // Outputting recieved Integer Object.
+            System.out.println("Received integer: " + i);
+            System.out.println("Received integer: " + i2);
+            out.close();
+            in.close();
+            out2.close();
+            in2.close();
+            clientSocket.close();
+            clientSocket2.close();
         }
     }
 
